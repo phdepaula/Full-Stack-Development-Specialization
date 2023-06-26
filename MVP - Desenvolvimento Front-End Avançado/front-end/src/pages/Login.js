b6f474d758b4d4e4caab08f3fbf5f2d053023e6f
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-06-25 22:46:30 -0300

    Configurando cookies da rota /login

--- MVP - Desenvolvimento Front-End Avançado/front-end/src/pages/Login.js
+++ MVP - Desenvolvimento Front-End Avançado/front-end/src/pages/Login.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 import Logo from '../components/Logo'
 import login from '../assets/login.svg'
+import { useNavigate } from 'react-router-dom';
 
 import '../style/login.css';
 
 function Login() {
+  const navigate = useNavigate();
+  const [isLogado, setIsLogado] = useState(false);
+
+  useEffect(() => {
+    const storedIsLogado = Cookies.get('isLogado');
+    
+    if (storedIsLogado === 'true') {
+      setIsLogado(true);
+    }
+  }, []);
+
   const verificar_login = (buttonId) => {
     let inputUsuario = document.getElementById("usuario").value;
     let inputSenha = document.getElementById("senha").value;
@@ -26,6 +39,9 @@ function Login() {
 
     if (resposta_api === 'True'){
       alert('Login realizado!')
+      setIsLogado(true);
+      Cookies.set('isLogado', 'true');
+      navigate('/')
     }
     else {
       alert('Dados Incorretos!')
@@ -37,39 +53,52 @@ function Login() {
 
     if (resposta_api === 'True'){
       alert('Conta criada com sucesso!')
+      setIsLogado(true);
+      Cookies.set('isLogado', 'true');
+      navigate('/')
     }
     else {
       alert('Erro ao criar conta!')
     }
   }
+  
+  if (isLogado === true) {
+    navigate('/')
+  }
 
   return (
-    <div className='Login'>
-      <section className='SecaoDireita'>
-        <div className='FrameDireita'>
-          <Logo />
-          <p id='FraseEmpresa'> Descubra a excelência em compras: nosso mercado online, onde qualidade e conveniência se encontram!</p>
+    <div>
+      {isLogado ? (
+        navigate('/')
+      ): (
+        <div className='Login'>
+          <section className='SecaoDireita'>
+            <div className='FrameDireita'>
+              <Logo />
+              <p id='FraseEmpresa'> Descubra a excelência em compras: nosso mercado online, onde qualidade e conveniência se encontram!</p>
+            </div>
+          </section >
+
+          <section className='SecaoEsquerda'>
+            <div className='FrameEsquerda'>
+              <div className='TituloLogin'>
+                <img src={ login } alt='Login' />
+                <p>Login</p>
+              </div>
+
+              <div className='EntradaTexto'>
+                <input type='text' id='usuario' placeholder='Usuário' required='required' autocomplete='off'/>
+                <input type='text' id='senha' placeholder='Senha' required='required' autocomplete='off'/>
+              </div>
+
+              <div className='Botoes'>
+                <button id='entrar' onClick= {() => verificar_login('entrar')}>ENTRAR</button>
+                <button id='criar-conta' onClick= {() => verificar_login('criar-conta')}>CRIAR CONTA</button>
+              </div>
+            </div>
+          </section >
         </div>
-      </section >
-
-      <section className='SecaoEsquerda'>
-        <div className='FrameEsquerda'>
-          <div className='TituloLogin'>
-            <img src={ login } alt='Login' />
-            <p>Login</p>
-          </div>
-
-          <div className='EntradaTexto'>
-            <input type='text' id='usuario' placeholder='Usuário' required='required' autocomplete='off'/>
-            <input type='text' id='senha' placeholder='Senha' required='required' autocomplete='off'/>
-          </div>
-
-          <div className='Botoes'>
-            <button id='entrar' onClick= {() => verificar_login('entrar')}>ENTRAR</button>
-            <button id='criar-conta' onClick= {() => verificar_login('criar-conta')}>CRIAR CONTA</button>
-          </div>
-        </div>
-      </section >
+      )}
     </div>
   );
 }
