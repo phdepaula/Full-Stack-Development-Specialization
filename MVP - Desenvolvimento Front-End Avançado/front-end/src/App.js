ac2c1e45fc19dafe2d881046910b7d8947b9d29f
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-06-25 21:56:16 -0300

    Adicionando rotas com React Router

--- MVP - Desenvolvimento Front-End Avançado/front-end/src/App.js
+++ MVP - Desenvolvimento Front-End Avançado/front-end/src/App.js
@@ -1,9 +1,18 @@
-import Login from './pages/Login'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import PaginaInicial from './pages/PaginaInicial';
+import Login from './pages/Login';
+import Produto from './pages/Produto';
+import PaginaNaoEncontrada from './pages/PaginaNaoEncontrada';
 
 export default function App() {
   return (
-    <div>
-      <Login />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<PaginaInicial />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/produto" element={<Produto />} />
+      <Route path="*" element={<PaginaNaoEncontrada />} />
+    </Routes>
+  );
 }
---
+++ MVP - Desenvolvimento Front-End Avançado/front-end/src/pages/PaginaNaoEncontrada.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PaginaNaoEncontrada = () => {
+  return <h1>Página não encontrada</h1>;
+};
+
+export default PaginaNaoEncontrada;
