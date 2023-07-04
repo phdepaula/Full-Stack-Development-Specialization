efa4e6afc6090c39b212abe788c0d0609e4a0e22
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-07-03 23:53:47 -0300

    Configurando nova pagina de produtos

---
+++ MVP - Desenvolvimento Front-End Avançado/front-end/src/components/Produto-Detalhado.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+
+import { imagensProdutos } from "./Imagens"
+import adicionar from '../assets/general/mais.svg'
+import remover from '../assets/general/menos.svg'
+
+export default function ProdutoDetalhado(props) {
+  const produto = props.produto.produto
+  const compras = props.quantidadeCompras;
+  const cancelarPagamento = props.cancelarPagamento
+  const imagem = imagensProdutos[produto.id]
+  const [quantidade, setQuantidade] = useState(0);
+  
+  let valor = Math.round((quantidade*produto.preco))
+
+  const mais = () => {
+    setQuantidade(quantidade + 1);
+  };
+
+  const menos = () => {
+    if (quantidade > 0) {
+      setQuantidade(quantidade - 1);
+    } else {
+      alert('A quantidade mínima do produto é 0!')
+    }
+  };
+
+  return (
+    <div className='Produto'>
+      <div className='DadosProdutos'>
+        <div className='ImagemProdutoDetalhado'>
+          <img src={imagem} alt='Imagem do Produto'/>
+        </div>
+
+        <div className='InformacoesAdicionais'>
+          <span id='TituloProduto'>{produto.nome}</span>
+          <span id='Detalhamento'>{produto.detalhamento}</span>
+
+          <div className='Estrutura'>
+            <div className='Topicos'>
+              <div className='Categoria'>
+                <span id='categoria'>Categoria:</span>
+                <span id='nomecategoria'>{produto.categoria}</span>
+              </div>
+              <div className='Fornecedor'>
+                <span id='fornecedor'>Fornecedor:</span>
+                <span id='nomefornecedor'>{produto.fornecedor}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='Pagamento'>
+        <span id='TituloPagamento'>Pagamento</span>
+
+        <div className='InformacoesPagamento'>
+          <div className='PrecoPagamentoObsevacao'>
+            <div className='PrecoPagamento'>
+              <span id='precopagamento'>Preço:</span>
+              <span id='valorunitario'>R$ {produto.preco}</span>
+            </div>
+
+            <div className='Observacao'>
+              <span id='observacao'>{produto.descricao}</span>
+            </div>
+          </div>
+
+          <div className='GrupoQuantidadePreco'>
+            <div className='PrecoTotal'>
+              <span id='total'>Total:</span>
+              <span id='valortotal'>R$ {valor}</span>
+            </div>
+
+            <div className='QuantidadePagamento'>
+              <span id='TextoQuantidadePagamento'>Quantidade:</span>
+              <span id='ItensQuantidadePagamento'>{quantidade}</span>
+              <img src={adicionar} alt="mais" onClick={mais}/>
+              <img src={remover} alt="menos" onClick={menos}/>
+            </div>
+          </div>
+
+          <div className='ButtonsPagamento'>
+            <button id='ButtonComprarPagamento' onClick={() => compras(quantidade, produto.preco)}>Comprar</button>
+            <button id='ButtonCancelarPagamento' onClick={() => cancelarPagamento(quantidade, produto.preco)}>Cancelar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- MVP - Desenvolvimento Front-End Avançado/front-end/src/pages/Produto.js
+++ MVP - Desenvolvimento Front-End Avançado/front-end/src/pages/Produto.js
@@ -1,14 +1,147 @@
-import React from 'react';
+import Cookies from 'js-cookie';
+import React, { useState }  from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
-function Produto() {
+import Header from '../components/Header'
+import Navegacao from '../components/Navegacao'
+import Rodape from '../components/Rodape'
+import ProdutoDetalhado from '../components/Produto-Detalhado';
+import CardProduto from '../components/Card-Produto'
+
+import produtos from '../produtos.json'
+import existe_produto from '../existe_produto.json'
+import '../style/produtos.css'
+
+import PaginaNaoEncontrada from './PaginaNaoEncontrada';
+
+export default function PaginaInicial() {
+  const cookieNomeUsuario = Cookies.get('nomeUsuario');
+  const { nome } = useParams();
+  const status_produto = existe_produto.status
+  const navigate = useNavigate();
+  
+  const [produtosLista, setProdutosLista] = useState(produtos.produtos);
+  const [categoria, setCategoria] = useState('Destaques');
+  const [quantidade, setQuantidade] = useState(0);
+  const [valorTotal, setValorTotal] = useState(0);
+  const [scrollPosicao, setScrollPosicao] = useState(0);
+
+  const larguratela = window.innerWidth;
+  const numeroCards = produtosLista.length;
+  const numeroCardsVisiveis = Math.round((larguratela/(221)));
+  let [cardAtual, setCardAtual] = useState(numeroCardsVisiveis)
+
+  const tipoCategoria = (infoCategoria) => {
+    setCategoria(infoCategoria)
+  }
+  
+  const quantidadeCompras = (numCompras, preco) => {
+    if(cookieNomeUsuario) {
+      const valor = Math.round((numCompras*preco))
+
+      if (window.confirm(`Deseja adicionar a compra no valor de R$ ${valor} ao carrinho?`)) {
+        setQuantidade(quantidade + numCompras)
+        setValorTotal(valorTotal + valor)
+        alert('Compra adicionada ao carrinho!')
+      }
+    } else {
+      alert('É necessário realizar login para adicionar compras ao carrinho!')
+    }    
+  }
+
+  const finalizarCompra = () => {
+    if (quantidade === 0) {
+      alert('Não existem items no carrinho!')
+    } else if (window.confirm(`Deseja realizar a compra no valor de R$ ${valorTotal}?`)) {
+      setValorTotal(0)
+      setQuantidade(0);
+      alert('Compra realizada com sucesso!')
+    } else if (window.confirm(`Deseja cancelar a compra no valor de R$ ${valorTotal}?`)) {
+      setValorTotal(0)
+      setQuantidade(0);
+      alert('Compra cancelada!')
+    } else {
+      alert('Continue comprando!')
+    }
+  }
+
+  const cancelarPagamento = (quantidadeCancelada, preco) => {
+    if (window.confirm(`Deseja cancelar ${quantidadeCancelada} compras(s) de ${nome}?`)) {
+      const valorCancelado = Math.round((quantidadeCancelada*preco))
+      
+      alert(`Compra(s) canceladas!`)
+      setQuantidade(quantidade - quantidadeCancelada);
+      setValorTotal(valorTotal - valorCancelado)
+    } else {
+      alert('Continue comprando!')
+    }
+  }
+  
+  const scrollEsquerda = () => {
+    const novaPosicao = scrollPosicao - 221
+
+    if(cardAtual > numeroCardsVisiveis) {
+      setCardAtual(cardAtual - 1)
+      setScrollPosicao(novaPosicao)
+    }
+  };
+
+  const scrollDireita = () => {
+    const novaPosicao = scrollPosicao + 221
+
+    if(cardAtual <= numeroCards) {
+      setCardAtual(cardAtual + 1)
+      setScrollPosicao(novaPosicao)
+    }
+  }
+
   return (
-    <div>
-      <h1>Produto</h1>
-      <p>Se você tiver alguma dúvida, sugestão ou feedback, não hesite em entrar em contato conosco.</p>
-      <p>Você pode nos enviar um e-mail, ligar para o nosso número de telefone ou preencher o formulário de contato.</p>
-      <p>Estamos ansiosos para ouvir de você e responderemos o mais rápido possível.</p>
+    status_produto === false ? <PaginaNaoEncontrada/>:
+    <div className='Page'>
+      <header>
+        <div className='Topo'>
+          <Header quantidade={quantidade} comprar={finalizarCompra} />
+        </div>
+        
+        <div className='Navegacao'>
+          <Navegacao tipoCategoria={tipoCategoria} />
+        </div>
+      </header>
+
+      <main>
+        <div className='Body'>
+          <div className='LocalizadorPagina'>
+            <span id='LocalizadorInicial'onClick={() => navigate('/')}>Pagina Inicial</span>
+            <span>{">"}</span>
+            <span>{nome}</span>
+          </div>
+
+          <div className='ProdutosDetalhados'>
+            <ProdutoDetalhado produto={existe_produto} quantidadeCompras={quantidadeCompras} cancelarPagamento={cancelarPagamento}/>
+          </div>
+
+          <div className='TituloCards'>
+            <span>{categoria}</span>
+          </div>
+
+          <div className='Cards'>
+            <div className='ListaProduto' style={{ transform: `translateX(-${scrollPosicao}px)`, flexGrow: 1 }}>
+                {produtosLista.map((p, index) => (
+                <CardProduto key={index} produto={p} quantidadeCompras={quantidadeCompras}/>
+              ))}
+            </div>
+
+            <div className='SetasProdutos'>
+              <button id='scrollEsquerda' onClick={scrollEsquerda}>&lt;</button>
+              <button id='scrollDireita' onClick={scrollDireita}>&gt;</button>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <Rodape />
+      </footer>
     </div>
   );
 }
-
-export default Produto;
