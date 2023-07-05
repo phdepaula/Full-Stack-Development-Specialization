c5d2bac8f3eacc8f81ab90704277e22eadbd310c
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-07-04 22:33:51 -0300

    Ajustando configurações gerais

--- MVP - Desenvolvimento Front-End Avançado/back-end/database/model/config_model.py
+++ MVP - Desenvolvimento Front-End Avançado/back-end/database/model/config_model.py
@@ -2,8 +2,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+# Classe Base para o instanciamento de novos objetos/tabelas
 Base = declarative_base()
 
+# Configuracoes do Banco de Dados (Database)
 db_path = "database/"
 db_url = 'sqlite:///%s/db.sqlite3' % db_path
 engine = create_engine(db_url, echo=False, connect_args={"check_same_thread": False})
