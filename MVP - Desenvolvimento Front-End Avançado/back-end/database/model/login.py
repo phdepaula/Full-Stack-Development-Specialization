87a4770b02870679fd468dfe943f3727e15b2971
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-07-04 21:52:56 -0300

    Criando tabela de login

---
+++ MVP - Desenvolvimento Front-End Avançado/back-end/database/model/login.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+
+from database.model.config_model import Base
+
+class Login(Base):
+    __tablename__ = 'login'
+
+    id_login = Column("id_login", Integer, primary_key=True, autoincrement=True)
+    usuario = Column("usuario", String(15), unique=True, nullable=False)
+    senha = Column(String(15), nullable=False)
+
+    def __init__(self, usuario: str, senha: str) -> None:
+        self.usuario = usuario
+        self.senha = senha
