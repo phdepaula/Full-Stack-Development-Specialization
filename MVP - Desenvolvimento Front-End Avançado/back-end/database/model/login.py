68ad18c0b5fda7759c96029384b288d46499022a
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-07-08 13:42:32 -0300

    Ajustando tamanho colunas tabela de login

--- MVP - Desenvolvimento Front-End Avançado/back-end/database/model/login.py
+++ MVP - Desenvolvimento Front-End Avançado/back-end/database/model/login.py
@@ -7,8 +7,8 @@ class Login(Base):
   __tablename__ = 'login'
   
   id_login = Column("id_login", Integer, primary_key=True, autoincrement=True)
-  usuario = Column("usuario", String(15), unique=True, nullable=False)
-  senha = Column(String(15), nullable=False)
+  usuario = Column("usuario", String(50), unique=True, nullable=False)
+  senha = Column(String(50), nullable=False)
   
   
   def __init__(self, usuario: str, senha: str) -> None:
